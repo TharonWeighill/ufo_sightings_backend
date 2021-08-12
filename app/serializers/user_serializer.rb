27619dfac773c5a9f8129b93ee4966dbc9989cb6
tharon <tharonweighill@gmail.com>
2021-08-12 16:40:52 -0600

    add serialiazers

---
+++ app/serializers/user_serializer.rb
@@ -0,0 +1,7 @@
+class UserSerializer < ActiveModel::Serializer
+    attributes :id, :email, :todos, :created_date
+  
+    def created_date
+      self.object && self.object.created_at.strftime('%d/%m/%Y')
+    end
+  end
