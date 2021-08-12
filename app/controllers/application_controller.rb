d9340c8110e943e0a840eea6c78850f5eda5d865
tharon <tharonweighill@gmail.com>
2021-08-12 16:38:35 -0600

    add auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,9 @@
 class ApplicationController < ActionController::API
-    # config/initializers/cors.rb
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
-    allow do
-      origins '*'
-      resource(
-       '*',
-       headers: :any,
-       expose: ["Authorization"],
-       methods: [:get, :patch, :put, :delete, :post, :options, :show]
-      )
-    end
-  end
 
+  protected
+      def configure_permitted_parameters
+          devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password, :password_confirmation, :remember_me) }
+          devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:email, :password, :remember_me) }
+          devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:email, :password, :password_confirmation, :current_password) }
+      end
 end
