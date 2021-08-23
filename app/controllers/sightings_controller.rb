2ec446b497ab73b17414ed4dbf64555264cbd98b
tharon <tharonweighill@gmail.com>
2021-08-23 11:47:23 -0600

    fix sightings params

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -47,7 +47,7 @@ class SightingsController < ApplicationController
   
      
       def sighting_params
-        params.require(:sightings).permit(:city, :state, :country, :comments, :date_posted, :latitude, :longitutde)
+        params.require(:sightings).permit(:city, :state, :country, :comments, :date_posted, :latitude, :longitude)
       end
   end
   
